dc9d940573b23ed349376de661e8476ca6ff89fb
charlesjunior91 <charles.rodrigues91@gmail.com>
2016-03-09 20:08:51 -0300

    Delete Fita_lancamento.java

--- modelo/Fita_lancamento.java
+++
@@ -1,8 +0,0 @@
-package br.unipe.mlpIII.locadora.modelo;
-
-public class Fita_lancamento extends Fitas {
-
-	public void setPreco(double preco) {
-		this.setPreco(preco + ((preco * 20) / 100));
-	}
-}
